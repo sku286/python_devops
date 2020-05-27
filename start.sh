0ceb1ae57b7eb7a96b250eba251646d3e7d85555
surendra kumar <surendra.kumar@nagarro.com>
2020-05-27 10:52:06 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,16 +3,10 @@
 # defining the branch to pull code
 BRANCH=${1:-dev}
 
-mkdir kiwee
-cd ./kiwee
-
 # clone all repos required
 git clone --single-branch --branch "$BRANCH" git@gitlab.com:kiwee_core/kiwee_connector.git
 git clone --single-branch --branch "$BRANCH" git@gitlab.com:kiwee_core/kiwee_dashboard_api.git
 git clone --single-branch --branch "$BRANCH" git@gitlab.com:kiwee_core/kiwee_gui.git
-
-# copy docker-compose file to kiwee folder
-cp ../docker-compose.yml .
 
 # running the containers
 docker-compose up
